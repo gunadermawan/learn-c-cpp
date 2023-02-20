f26354b6b4f6e48737d0756ccf8da9045f0360d7
gunadermawan <gunadermawan.official@gmail.com>
2023-02-20 09:50:01 +0700

    add simple algorithm and create variabel in c

---
+++ Main/main.c
@@ -0,0 +1,21 @@
+// library
+#include <stdio.h>
+
+//code program
+int main(void) {
+    int value = 10;
+    int otherValue = 20;
+    printf("hi there, its just ice cream!\n");
+    printf("the value is: %d\n", value * otherValue);
+
+//    create simple algorithm
+    value = value + 10;
+    printf("now value is: %d\n", value);
+}
+
+//with return value
+
+//int main(){
+//    printf("hi there, its just ice cream!\n");
+//    return  0;
+//}
--- main.c
+++
@@ -1,7 +0,0 @@
-// library
-#include <stdio.h>
-
-//code program
-int main(void) {
-    printf("hi there, its just ice cream!\n");
-}
